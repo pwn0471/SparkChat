0ef1849ade9281550d407a4091467e173087be90
pawankumar <pwnkumar0471@gmail.com>
2026-02-23 17:25:30 +0530

    signup

--- server/src/controllers/authController.js
+++ server/src/controllers/authController.js
@@ -1,5 +1,59 @@
+import bcrypt from "bcryptjs";
+import User from "../models/User.js"
+import jwt from "jsonwebtoken"
+
 export  async function signup(req,res){
-    res.send("signup route");
+    const {email, password, fullName}= req.body;
+
+    try{
+        if(!email || !password || !fullName){
+            return res.status(400).json({message : "All fields are required "});
+        }
+
+        if(password.length < 6){
+            return res.status(400).json({message:"Password must be at least 6 character "});
+        }
+
+        const emailRegex  = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+        if(!emailRegex.test(email)){
+            return res.status(400).json({message:"Invalid email format "});
+        }
+        const exitingUser = await User.findOne({email});
+        if(exitingUser){
+            return res.status(400).json({message:"Email already exits , please enter diifferent email"});
+        }
+        
+        const idx = Math.floor(Math.random()*100) + 1;
+        const randomAvatar = `https://avatar.iran.liara.run/public/${idx}.png`
+
+        const newUser = await User.create({
+            email,
+            fullName,
+            password,
+            profilePic: randomAvatar,
+
+        })
+
+        // todo creTE user in stream as well
+
+        const token = jwt.sign({userId:newUser._id},process.env.JWT_SECRET_KEY,{
+            expiresIn: "7d"
+        })
+
+        res.cookie("jwt",token,{
+            maxAge:7*24*60*60*1000,
+            httpOnly: true,         // prevent xss attacks
+            sameSite: "strict",  // prevent csrf attacks
+            secure : process.env.NODE_ENV === "production",
+        })
+
+        res.status(201).json({success:true, user:newUser})
+
+    }catch(error){
+        console.log("error in signup cintroller", error);
+        res.status(500).json({message:"Internal server error"});
+    }
 }
 
 export async function login(req,res){
---
+++ server/src/models/User.js
@@ -0,0 +1,71 @@
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs"
+const userSchema = new mongoose.Schema(
+    {
+        fullName: {
+            type: String,
+            required: true,
+        },
+        email:{
+            type:String,
+            required:true,
+            unique:true,
+        },
+        password:{
+            type:String,
+            required:true,
+            minlength:6,
+        },
+        bio:{
+            type:String,
+            default:"",
+
+
+        },
+        profilePic:{
+            type:String,
+            default:"",
+
+        },
+        nativeLanguage:{
+            type:String,
+            default:"",
+        },
+        learningLanguage:{
+            type:String,
+            deafult:"",
+        
+        },
+        location:{
+            type:String,
+            default:false,
+
+        },
+        friends:[
+            {
+                type:mongoose.Schema.Types.ObjectId,
+                ref:"User",
+            }
+        ]
+    },{timestamps:true}
+);
+
+
+// pre hook
+
+userSchema.pre("save", async function(){
+    if(!this.isModified("password")) return ;
+    try{
+        const salt = await bcrypt.genSalt(10);
+        this.password = await bcrypt.hash(this.password,salt);
+        
+
+
+    }catch (error){
+        console.log(error)
+    }
+});
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
