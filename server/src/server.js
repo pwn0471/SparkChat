80712153070cb543563bf5083209571a9d36698c
pawankumar <pwnkumar0471@gmail.com>
2026-03-21 22:03:15 +0530

    production

--- server/src/server.js
+++ server/src/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import cookiesParser from "cookie-parser";
 import cors from "cors";
+import path from "path";
 
 import authRoute from "./routes/authRoute.js"
 import userRoutes from "./routes/userRoutes.js"
@@ -11,6 +12,7 @@ import { connectDB } from "./lib/db.js";
 dotenv.config();
 
 const app = express()
+const __dirname = path.resolve();
 
 app.use(cors({
     origin:"http://localhost:5173",
@@ -23,6 +25,18 @@ app.use(cookiesParser());
 app.use("/api/auth",authRoute);
 app.use("/api/users", userRoutes);
 app.use("/api/chat", chatRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  const clientPath = path.join(__dirname, "../client/dist");
+
+  app.use(express.static(clientPath));
+
+  // Catch all non-API routes
+  app.get("/{*any}", (req, res) => {
+    res.sendFile(path.join(clientPath, "index.html"));
+  });
+}
+
 
 const PORT = process.env.PORT 
 app.listen(PORT,()=>{
